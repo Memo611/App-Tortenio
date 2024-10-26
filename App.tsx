28f2977d67bb3259d3286598e5dd7095fe6b8392
Guillermo Garcia Torres <memo777torres@gmail.com>
2024-10-25 18:32:30 -0700

    Base de inicio de secion

--- App.tsx
+++ App.tsx
@@ -2,14 +2,35 @@ import React from "react";
 import {
   View,
   Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button
 } from 'react-native';
 
 export default function App() {
   return (
-  <View>
-    <Text>
-      Tortenio
-    </Text>
-  </View>
+    <View style={styles.container}>
+      <Text style={styles.text}> Tortenio </Text>
+      <Text>Usuario: </Text>
+      <TextInput placeholder="Escribe aqui"></TextInput>
+      <Text>Contraseña: </Text>
+      <TextInput placeholder="Escribe aqui"></TextInput>
+      <Button title="Iniciar Secion" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'orange', // Aquí estableces el fondo naranja
+  },
+  text: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
